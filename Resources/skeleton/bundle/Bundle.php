af55faa0582bc87d2ba87cb52032ece638058dc1
Fabien Potencier <fabien.potencier@gmail.com>
2010-07-09 10:05:26 +0200

    renamed Symfony\Bundle\FoundationBundle to Symfony\Bundle\FrameworkBundle

---
+++ Resources/skeleton/bundle/Bundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace {{ namespace }}\{{ bundle }};
+
+use Symfony\Foundation\Bundle\Bundle;
+use Symfony\Components\DependencyInjection\ContainerInterface;
+
+class {{ bundle }} extends Bundle
+{
+}
