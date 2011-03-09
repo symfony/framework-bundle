b2df54fe75dd76a41f1680b429ca77a4c611220b
umpirsky <umpirsky@gmail.com>
2011-03-09 15:14:01 +0100

    Validators messages translated to Serbian language.

---
+++ Resources/views/Form/form.html.php
@@ -0,0 +1,9 @@
+<?php echo $view['form']->errors($field) ?>
+
+<div>
+    <?php foreach ($field->getVisibleFields() as $child): ?>
+        <?php echo $view['form']->row($child) ?>
+    <?php endforeach; ?>
+</div>
+
+<?php echo $view['form']->hidden($field) ?>
