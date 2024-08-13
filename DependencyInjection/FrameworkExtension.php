francisrivas/Symfony

decouple the Webhook component from the Serializer component

src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
@@ -548,7 +548,7 @@ class FrameworkExtension extends Extension
         $this->registerProfilerConfiguration($config['profiler'], $container, $loader);
 
         if ($this->readConfigEnabled('webhook', $container, $config['webhook'])) {
-            $this->registerWebhookConfiguration($config['webhook'], $container, $loader);
+            $this->registerWebhookConfiguration($config['webhook'], $container, $loader, $this->readConfigEnabled('serializer', $container, $config['serializer']));
 
             // If Webhook is installed but the HttpClient or Serializer components are not available, we should throw an error
             if (!$this->readConfigEnabled('http_client', $container, $config['http_client'])) {
@@ -556,14 +556,6 @@ class FrameworkExtension extends Extension
                     ->setArguments([])
                     ->addError('You cannot use the "webhook transport" service since the HttpClient component is not '
                         .(class_exists(ScopingHttpClient::class) ? 'enabled. Try setting "framework.http_client.enabled" to true.' : 'installed. Try running "composer require symfony/http-client".')
-                    )
-                    ->addTag('container.error');
-            }
-            if (!$this->readConfigEnabled('serializer', $container, $config['serializer'])) {
-                $container->getDefinition('webhook.body_configurator.json')
-                    ->setArguments([])
-                    ->addError('You cannot use the "webhook transport" service since the Serializer component is not '
-                        .(class_exists(Serializer::class) ? 'enabled. Try setting "framework.serializer.enabled" to true.' : 'installed. Try running "composer require symfony/serializer-pack".')
                     )
                     ->addTag('container.error');
             }
@@ -2919,7 +2911,7 @@ class FrameworkExtension extends Extension
         }
     }
 
-    private function registerWebhookConfiguration(array $config, ContainerBuilder $container, PhpFileLoader $loader): void
+    private function registerWebhookConfiguration(array $config, ContainerBuilder $container, PhpFileLoader $loader, bool $serializerEnabled): void
     {
         if (!class_exists(WebhookController::class)) {
             throw new LogicException('Webhook support cannot be enabled as the component is not installed. Try running "composer require symfony/webhook".');
@@ -2938,6 +2930,9 @@ class FrameworkExtension extends Extension
         $controller = $container->getDefinition('webhook.controller');
         $controller->replaceArgument(0, $parsers);
         $controller->replaceArgument(1, new Reference($config['message_bus']));
+
+        $jsonBodyConfigurator = $container->getDefinition('webhook.body_configurator.json');
+        $jsonBodyConfigurator->replaceArgument(0, new Reference($serializerEnabled ? 'webhook.payload_encoder.serializer' : 'webhook.payload_encoder.json'));
     }
 
     private function registerRemoteEventConfiguration(PhpFileLoader $loader): void

src/Symfony/Bundle/FrameworkBundle/Resources/config/webhook.php
@@ -17,6 +17,8 @@ use Symfony\Component\Webhook\Messenger\SendWebhookHandler;
 use Symfony\Component\Webhook\Server\HeadersConfigurator;
 use Symfony\Component\Webhook\Server\HeaderSignatureConfigurator;
 use Symfony\Component\Webhook\Server\JsonBodyConfigurator;
+use Symfony\Component\Webhook\Server\NativeJsonPayloadEncoder;
+use Symfony\Component\Webhook\Server\SerializerPayloadEncoder;
 use Symfony\Component\Webhook\Server\Transport;
 
 return static function (ContainerConfigurator $container) {
@@ -32,6 +34,13 @@ return static function (ContainerConfigurator $container) {
         ->set('webhook.headers_configurator', HeadersConfigurator::class)
 
         ->set('webhook.body_configurator.json', JsonBodyConfigurator::class)
+            ->args([
+                abstract_arg('payload encoder'),
+            ])
+
+        ->set('webhook.payload_encoder.json', NativeJsonPayloadEncoder::class)
+
+        ->set('webhook.payload_encoder.serializer', SerializerPayloadEncoder::class)
             ->args([
                 service('serializer'),
             ])

src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/FrameworkExtensionTestCase.php
@@ -2357,7 +2357,7 @@ abstract class FrameworkExtensionTestCase extends TestCase
         $this->assertSame(RequestParser::class, $container->getDefinition('webhook.request_parser')->getClass());
 
         $this->assertFalse($container->getDefinition('webhook.transport')->hasErrors());
-        $this->assertFalse($container->getDefinition('webhook.body_configurator.json')->hasErrors());
+        $this->assertEquals('webhook.payload_encoder.serializer', $container->getDefinition('webhook.body_configurator.json')->getArgument(0));
     }
 
     public function testWebhookWithoutSerializer()
@@ -2369,11 +2369,7 @@ abstract class FrameworkExtensionTestCase extends TestCase
         $container = $this->createContainerFromFile('webhook_without_serializer');
 
         $this->assertFalse($container->getDefinition('webhook.transport')->hasErrors());
-        $this->assertTrue($container->getDefinition('webhook.body_configurator.json')->hasErrors());
-        $this->assertSame(
-            ['You cannot use the "webhook transport" service since the Serializer component is not enabled. Try setting "framework.serializer.enabled" to true.'],
-            $container->getDefinition('webhook.body_configurator.json')->getErrors()
-        );
+        $this->assertEquals('webhook.payload_encoder.json', $container->getDefinition('webhook.body_configurator.json')->getArgument(0));
     }
 
     public function testAssetMapperWithoutAssets()

symfony/framework-bundle

DependencyInjection/FrameworkExtension.php
@@ -548,7 +548,7 @@ class FrameworkExtension extends Extension
         $this->registerProfilerConfiguration($config['profiler'], $container, $loader);
 
         if ($this->readConfigEnabled('webhook', $container, $config['webhook'])) {
-            $this->registerWebhookConfiguration($config['webhook'], $container, $loader);
+            $this->registerWebhookConfiguration($config['webhook'], $container, $loader, $this->readConfigEnabled('serializer', $container, $config['serializer']));
 
             // If Webhook is installed but the HttpClient or Serializer components are not available, we should throw an error
             if (!$this->readConfigEnabled('http_client', $container, $config['http_client'])) {
@@ -556,14 +556,6 @@ class FrameworkExtension extends Extension
                     ->setArguments([])
                     ->addError('You cannot use the "webhook transport" service since the HttpClient component is not '
                         .(class_exists(ScopingHttpClient::class) ? 'enabled. Try setting "framework.http_client.enabled" to true.' : 'installed. Try running "composer require symfony/http-client".')
-                    )
-                    ->addTag('container.error');
-            }
-            if (!$this->readConfigEnabled('serializer', $container, $config['serializer'])) {
-                $container->getDefinition('webhook.body_configurator.json')
-                    ->setArguments([])
-                    ->addError('You cannot use the "webhook transport" service since the Serializer component is not '
-                        .(class_exists(Serializer::class) ? 'enabled. Try setting "framework.serializer.enabled" to true.' : 'installed. Try running "composer require symfony/serializer-pack".')
                     )
                     ->addTag('container.error');
             }
@@ -2919,7 +2911,7 @@ class FrameworkExtension extends Extension
         }
     }
 
-    private function registerWebhookConfiguration(array $config, ContainerBuilder $container, PhpFileLoader $loader): void
+    private function registerWebhookConfiguration(array $config, ContainerBuilder $container, PhpFileLoader $loader, bool $serializerEnabled): void
     {
         if (!class_exists(WebhookController::class)) {
             throw new LogicException('Webhook support cannot be enabled as the component is not installed. Try running "composer require symfony/webhook".');
@@ -2938,6 +2930,9 @@ class FrameworkExtension extends Extension
         $controller = $container->getDefinition('webhook.controller');
         $controller->replaceArgument(0, $parsers);
         $controller->replaceArgument(1, new Reference($config['message_bus']));
+
+        $jsonBodyConfigurator = $container->getDefinition('webhook.body_configurator.json');
+        $jsonBodyConfigurator->replaceArgument(0, new Reference($serializerEnabled ? 'webhook.payload_serializer.serializer' : 'webhook.payload_serializer.json'));
     }
 
     private function registerRemoteEventConfiguration(PhpFileLoader $loader): void

Resources/config/webhook.php
@@ -17,6 +17,8 @@ use Symfony\Component\Webhook\Messenger\SendWebhookHandler;
 use Symfony\Component\Webhook\Server\HeadersConfigurator;
 use Symfony\Component\Webhook\Server\HeaderSignatureConfigurator;
 use Symfony\Component\Webhook\Server\JsonBodyConfigurator;
+use Symfony\Component\Webhook\Server\NativeJsonPayloadSerializer;
+use Symfony\Component\Webhook\Server\SerializerPayloadSerializer;
 use Symfony\Component\Webhook\Server\Transport;
 
 return static function (ContainerConfigurator $container) {
@@ -32,6 +34,13 @@ return static function (ContainerConfigurator $container) {
         ->set('webhook.headers_configurator', HeadersConfigurator::class)
 
         ->set('webhook.body_configurator.json', JsonBodyConfigurator::class)
+            ->args([
+                abstract_arg('payload serializer'),
+            ])
+
+        ->set('webhook.payload_serializer.json', NativeJsonPayloadSerializer::class)
+
+        ->set('webhook.payload_serializer.serializer', SerializerPayloadSerializer::class)
             ->args([
                 service('serializer'),
             ])

Tests/DependencyInjection/FrameworkExtensionTestCase.php
@@ -2357,7 +2357,7 @@ abstract class FrameworkExtensionTestCase extends TestCase
         $this->assertSame(RequestParser::class, $container->getDefinition('webhook.request_parser')->getClass());
 
         $this->assertFalse($container->getDefinition('webhook.transport')->hasErrors());
-        $this->assertFalse($container->getDefinition('webhook.body_configurator.json')->hasErrors());
+        $this->assertEquals('webhook.payload_serializer.serializer', $container->getDefinition('webhook.body_configurator.json')->getArgument(0));
     }
 
     public function testWebhookWithoutSerializer()
@@ -2369,11 +2369,7 @@ abstract class FrameworkExtensionTestCase extends TestCase
         $container = $this->createContainerFromFile('webhook_without_serializer');
 
         $this->assertFalse($container->getDefinition('webhook.transport')->hasErrors());
-        $this->assertTrue($container->getDefinition('webhook.body_configurator.json')->hasErrors());
-        $this->assertSame(
-            ['You cannot use the "webhook transport" service since the Serializer component is not enabled. Try setting "framework.serializer.enabled" to true.'],
-            $container->getDefinition('webhook.body_configurator.json')->getErrors()
-        );
+        $this->assertEquals('webhook.payload_serializer.json', $container->getDefinition('webhook.body_configurator.json')->getArgument(0));
     }
 
     public function testAssetMapperWithoutAssets()