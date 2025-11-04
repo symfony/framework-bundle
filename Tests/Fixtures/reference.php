58adaf5f8a381b176895b11fe9cc5ab792749c42
Nicolas Grekas <nicolas.grekas@gmail.com>
2025-11-04 14:31:53 +0100

    [Routing] Simplify importing routes defined on controller services

--- Tests/Fixtures/reference.php
+++ Tests/Fixtures/reference.php
@@ -184,8 +184,7 @@ namespace Symfony\Component\Routing\Loader\Configurator;
  *
  *     return Routes::config([
  *         'controllers' => [
- *             'resource' => 'attributes',
- *             'type' => 'tagged_services',
+ *             'resource' => 'routing.controllers',
  *         ],
  *     ]);
  *     ```
