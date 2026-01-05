chalasr/symfony

[Console] Add argument resolvers

src/Symfony/Bundle/FrameworkBundle/Tests/Fixtures/Console/CustomOptionValueResolver.php
@@ -1,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bundle\FrameworkBundle\Tests\Fixtures\Console;
+
+use Symfony\Component\Console\ArgumentResolver\ValueResolver\ValueResolverInterface;
+use Symfony\Component\Console\Attribute\Reflection\ReflectionMember;
+use Symfony\Component\Console\Input\InputInterface;
+
+class CustomOptionValueResolver implements ValueResolverInterface
+{
+    public function resolve(string $argumentName, InputInterface $input, ReflectionMember $member): iterable
+    {
+        $type = $member->getType();
+
+        if (!$type instanceof \ReflectionNamedType || CustomOption::class !== $type->getName()) {
+            return [];
+        }
+
+        $value = $input->hasOption('format') ? $input->getOption('format') : 'default';
+
+        yield new CustomOption('option:' . $value);
+    }
+}

src/Symfony/Bundle/FrameworkBundle/Tests/Fixtures/Console/TargetedCustomValueResolver.php
@@ -1,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bundle\FrameworkBundle\Tests\Fixtures\Console;
+
+use Symfony\Component\Console\ArgumentResolver\ValueResolver\ValueResolverInterface;
+use Symfony\Component\Console\Attribute\AsTargetedValueResolver;
+use Symfony\Component\Console\Attribute\Reflection\ReflectionMember;
+use Symfony\Component\Console\Input\InputInterface;
+
+#[AsTargetedValueResolver('targeted')]
+final class TargetedCustomValueResolver implements ValueResolverInterface
+{
+    public function resolve(string $argumentName, InputInterface $input, ReflectionMember $member): iterable
+    {
+        if ('targeted' === $argumentName && TargetedCustomType::class === $member->getType()?->getName()) {
+            return [new TargetedCustomType('targeted-value')];
+        }
+
+        return [];
+    }
+}

symfony/framework-bundle

Tests/Fixtures/Console/CustomOptionValueResolver.php
@@ -1,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bundle\FrameworkBundle\Tests\Fixtures\Console;
+
+use Symfony\Component\Console\ArgumentResolver\ValueResolver\ValueResolverInterface;
+use Symfony\Component\Console\Attribute\Reflection\ReflectionMember;
+use Symfony\Component\Console\Input\InputInterface;
+
+class CustomOptionValueResolver implements ValueResolverInterface
+{
+    public function resolve(string $argumentName, InputInterface $input, ReflectionMember $member): iterable
+    {
+        $type = $member->getType();
+
+        if ('customOption' !== $argumentName || !$type instanceof \ReflectionNamedType || CustomType::class !== $type->getName()) {
+            return [];
+        }
+
+        $value = $input->hasOption('format') ? $input->getOption('format') : 'default';
+
+        yield new CustomType('option:' . $value);
+    }
+}

Tests/Fixtures/Console/TargetedCustomValueResolver.php
@@ -1,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bundle\FrameworkBundle\Tests\Fixtures\Console;
+
+use Symfony\Component\Console\ArgumentResolver\ValueResolver\ValueResolverInterface;
+use Symfony\Component\Console\Attribute\AsTargetedValueResolver;
+use Symfony\Component\Console\Attribute\Reflection\ReflectionMember;
+use Symfony\Component\Console\Input\InputInterface;
+
+#[AsTargetedValueResolver('targeted')]
+final class TargetedCustomValueResolver implements ValueResolverInterface
+{
+    public function resolve(string $argumentName, InputInterface $input, ReflectionMember $member): iterable
+    {
+        if ('targeted' === $argumentName && CustomType::class === $member->getType()?->getName()) {
+            return [new CustomType('targeted-value')];
+        }
+
+        return [];
+    }
+}