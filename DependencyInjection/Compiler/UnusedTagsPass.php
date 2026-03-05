6e02f6ce45c6da2d3b9f74a0b60ec84c6fcf42c2
Nicolas Grekas <nicolas.grekas@gmail.com>
2026-03-05 17:24:49 +0100

    [DependencyInjection] Add support for using service stacks as decorators, including `decorates_tag`

--- DependencyInjection/Compiler/UnusedTagsPass.php
+++ DependencyInjection/Compiler/UnusedTagsPass.php
@@ -44,7 +44,6 @@ class UnusedTagsPass implements CompilerPassInterface
         'container.service_locator',
         'container.service_locator_context',
         'container.service_subscriber',
-        'container.stack',
         'controller.argument_value_resolver',
         'controller.service_arguments',
         'controller.targeted_value_resolver',
