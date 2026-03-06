dcd5019d62790227cdc6ac7c5487acdf1ba7de21
Nicolas Grekas <nicolas.grekas@gmail.com>
2026-03-06 09:30:41 +0100

    [FrameworkBundle] Remove hard-dep of KernelTestCase on ConsoleAssertionsTrait

--- Test/ConsoleCommandAssertionsTrait.php
+++ Test/ConsoleCommandAssertionsTrait.php
@@ -13,13 +13,13 @@ namespace Symfony\Bundle\FrameworkBundle\Test;
 
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Tester\CommandTester;
-use Symfony\Component\Console\Tester\ConsoleAssertionsTrait;
+use Symfony\Component\Console\Tester\Constraint\CommandFailed;
+use Symfony\Component\Console\Tester\Constraint\CommandIsInvalid;
+use Symfony\Component\Console\Tester\Constraint\CommandIsSuccessful;
 use Symfony\Component\Console\Tester\ExecutionResult;
 
 trait ConsoleCommandAssertionsTrait
 {
-    use ConsoleAssertionsTrait;
-
     /**
      * Runs a console command and returns the execution result.
      *
@@ -34,5 +34,45 @@ trait ConsoleCommandAssertionsTrait
         $commandTester = new CommandTester($command);
 
         return $commandTester->run($input, $interactiveInputs, $interactive, $decorated, $verbosity, $normalizers);
+    }
+
+    public function assertCommandIsSuccessful(ExecutionResult $result, string $message = ''): void
+    {
+        $this->assertThat($result->statusCode, new CommandIsSuccessful(), $message);
+    }
+
+    public function assertCommandFailed(ExecutionResult $result, string $message = ''): void
+    {
+        $this->assertThat($result->statusCode, new CommandFailed(), $message);
+    }
+
+    public function assertCommandIsInvalid(ExecutionResult $result, string $message = ''): void
+    {
+        $this->assertThat($result->statusCode, new CommandIsInvalid(), $message);
+    }
+
+    public function assertCommandResultEquals(ExecutionResult $result, ?int $expectedStatusCode = null, ?string $expectedOutput = null, ?string $expectedErrorOutput = null, ?string $expectedDisplay = null, string $message = ''): void
+    {
+        $expected = [];
+        $actual = [];
+
+        if (null !== $expectedStatusCode) {
+            $expected['statusCode'] = $expectedStatusCode;
+            $actual['statusCode'] = $result->statusCode;
+        }
+        if (null !== $expectedOutput) {
+            $expected['output'] = $expectedOutput;
+            $actual['output'] = $result->getOutput();
+        }
+        if (null !== $expectedErrorOutput) {
+            $expected['errorOutput'] = $expectedErrorOutput;
+            $actual['errorOutput'] = $result->getErrorOutput();
+        }
+        if (null !== $expectedDisplay) {
+            $expected['display'] = $expectedDisplay;
+            $actual['display'] = $result->getDisplay();
+        }
+
+        $this->assertEquals($expected, $actual, $message);
     }
 }
